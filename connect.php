9153aa3c8e7807c645fae36062d4664db5f70714
user <no.email>
2014-02-24 00:43:39 -0500

    changed connect.php password to nicolas82

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 $dbHost = "localhost";
 $dbUsername = "root";
-$dbPassword = "goldenbear";
+$dbPassword = "nicolas82";
 $dbConnect = mysql_connect($dbHost, $dbUsername, $dbPassword) or die (mysql_error());
 ?>
